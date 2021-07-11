4f4c50ffa6d37c52de4b3408194250d05fd4c7b0
betterUE <m17338148416@163.com>
2021-07-11 23:36:04 +0800

    测试github

---
+++ bio_demo/src/TestGithub.java
@@ -0,0 +1,10 @@
+/**
+ * @author : betterUE
+ * @create : 2021/7/11 23:34
+ * @desc : 测试github
+ */
+public class TestGithub {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
